1350218e5497809403e9c0a857c9cbdae3510f68
hoonyhoney <hoonyhoeny@gmail.com>
2023-05-18 00:24:43 +0900

    -피보나치수열

---
+++ src/algorithm/array/LeaderSelect.java
@@ -0,0 +1,13 @@
+package algorithm.array;
+
+public class LeaderSelect {
+    /*
+            1번 학생 2 3 1 7 3
+            2번 학생 4 1 9 6 8
+            3번 학생 5 5 2 4 4
+            4번 학생 6 5 2 6 7
+            5번 학생 8 4 2 2 2
+    */
+
+
+}
