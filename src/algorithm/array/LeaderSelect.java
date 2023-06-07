d2dcef39fb8cfe2bb3472e8e2d4f99d0ccad8051
hoonyhoney <hoonyhoeny@gmail.com>
2023-06-08 06:09:56 +0900

    -반장뽑기

--- src/algorithm/array/LeaderSelect.java
+++ src/algorithm/array/LeaderSelect.java
@@ -1,13 +1,39 @@
 package algorithm.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//반장구하기
 public class LeaderSelect {
-    /*
-            1번 학생 2 3 1 7 3
-            2번 학생 4 1 9 6 8
-            3번 학생 5 5 2 4 4
-            4번 학생 6 5 2 6 7
-            5번 학생 8 4 2 2 2
-    */
+
+    public static int solution(String str) {
+        int answer = 0, max=Integer.MIN_VALUE;
+        int[][] arr = {{2,3,1,7,3},{4,1,9,6,8}, {5,5,2,4,4}, {6,5,2,6,7}, {8,4,2,2,2}};
+
+        //
+
+        for(int i=0; i<arr[0].length; i++){
+            int cnt=0;
+            for(int j=0; j<arr.length; j++){
+                for(int k =0; k<=4; k++){
+                    if(arr[i][k]==arr[j][k]){
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt>max){
+                max=cnt;
+                answer=i;
+            }
+        }
 
 
+        return answer;
+    }
+
+
+    public static void main (String[]args){
+        System.out.println(solution("str"));
+    }
 }
